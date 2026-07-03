8c171ccc530c88416a97868b475f204c7ceac8c1
Ravi Tripathi <tripathiravi5500@gmail.com>
2026-07-04 04:49:24 +0530

    Js file change

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
 const addTodoBtn = document.getElementById("addTodoBtn")
 const inputTag = document.getElementById("todoInput")
+const todoListUl = document.getElementById("todoList")
+
 let todoText; //This should be populated when the user clicks on add button  
-// if we have todos in the localstorage we will read it 
 let todos = [];
 let todosString = localStorage.getItem("todos")
+// if we have todos in the localstorage we will read it 
 if (todosString) {
     todos = JSON.parse(todosString)
 }
 
+
+const populateTodos = ()=> {
+    let string = ""
+    for(const todo of todos) {
+        string += `<li id="todo-${todo.id}" class="todo-item ${todo.isCompleted? "completed" : ""}">
+        <input title="text" type="checkbox" class="todo-checkbox" ${todo.isCompleted? "checked" : ""} >
+        <span class="todo-text">${todo.title}</span>
+        <button class="delete-btn">×</button>
+        </li>` 
+    }
+    todoListUl.innerHTML = string
+}
+
+
 addTodoBtn.addEventListener("click", ()=> {
-    console.log("Hey i just clicked")
+      
     todoText = inputTag.value
-    console.log(todoText);
     inputTag.value = ""
     let todo = {
+       id: todos.length, 
        title: todoText,
        isCompleted: false    
     }
     todos.push(todo)
     localStorage.setItem("todos", JSON.stringify(todos))
+    populateTodos()
+})
+
+populateTodos()
+
+const todoCheckboxes = document.querySelectorAll(".todo-checkbox")
+
+todoCheckboxes.forEach((element)=> {
+    element.addEventListener("click", (e)=>{
+    if (e.target.checked) {
+        element.parentNode.classList.add("completed")
+        console.log(element.parentNode.id)
+        // grab this todo from todos array and update the todos array to set this todos isCompleted attributes as true 
+         todos = todos.map(todo =>{
+
+            if ("todo-"+ todo.id == element.parentNode.id) {
+                return {...todo, isCompleted: true}
+            }else{
+              
+                return todo
+            }
+         })
+        
+         localStorage.setItem("todos", JSON.stringify(todos))
+    }
+    else {
+        element.parentNode.classList.remove("completed")
+               // grab this todo from todos array and update the todos array to set this todos isCompleted attributes as true 
+         todos = todos.map(todo =>{
+          
+            if ("todo-"+ todo.id == element.parentNode.id) {
+                return {...todo, isCompleted: false}
+            }else{
+                
+                return todo
+            }
+         })
+      
+         localStorage.setItem("todos", JSON.stringify(todos))
+    }         
+    })
 })
